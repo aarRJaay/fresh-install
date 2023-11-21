8eb1da61574a5ef564b7f238011576d10bd94227
Rich South <richsouth@protonmail.com>
2023-11-21 15:11:16 +0000

    First

---
+++ dotfiles/.install/extras.sh
@@ -0,0 +1,16 @@
+packagesPacman=(
+    "audacity"
+    "firefox"
+    "kdeconnect"
+    "plasma-wayland-session"
+    "sddm"
+    "tailscale"
+    "thunderbird"
+    "zsh"
+);
+
+packagesYay=(
+    "quickemu"
+    "quickgui"
+    "wps-office"
+);

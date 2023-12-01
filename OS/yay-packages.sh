7745050500a190a7821036a03c2ef9587e5bc735
Rich South <richsouth@protonmail.com>
2023-12-01 08:17:56 +0000

    Added applications into shell scripts

---
+++ OS/yay-packages.sh
@@ -0,0 +1,1 @@
+yay -Syu pfetch  bibata-cursor-theme  neovim-git trizen swww  swaylock-effects  wlogout wlr-randr quickemu wps-office libreoffice protonmail-bridge neovide syncthing-gtk obsidian yt-dlp fend
